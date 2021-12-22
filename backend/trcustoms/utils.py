b60e0d9357925d83d7416904b5e0f4f1c073344e
rr- <dash@wind.garden>
2021-12-22 21:09:16 +0100

    scraper: add ability to not specify outer bounds

--- backend/trcustoms/utils.py
+++ backend/trcustoms/utils.py
@@ -1,5 +1,6 @@
 import re
 import uuid
+from collections.abc import Iterable
 from pathlib import Path
 
 from django.db import models
@@ -10,6 +11,37 @@ from rest_framework import status
 
 def slugify(text: str) -> str:
     return re.sub(r"[^\w\d]", "", text)
+
+
+def unbound_range(start: int) -> Iterable[int]:
+    num = start
+    while True:
+        yield num
+        num += 1
+
+
+def id_range(source: str) -> Iterable[Iterable[int]]:
+    """Take a string and convert it to a list of iterable ranges.
+
+    Example:
+
+    1,2:    [[1], [2]]
+    1..100: [range(1, 101)]
+    ..200:  [range(1, 201)]
+    200..:  [range(200, infinity)]
+    """
+    items = re.sub(r"\s+", "", source).split(",")
+    for item in items:
+        if match := re.fullmatch(r"(\d+)", item):
+            yield [int(match.group(1))]
+        elif match := re.fullmatch(r"(\d+)\.\.\.?(\d+)", item):
+            yield range(int(match.group(1)), int(match.group(2)) + 1)
+        elif match := re.fullmatch(r"\.\.\.?(\d+)", item):
+            yield range(1, int(match.group(1)) + 1)
+        elif match := re.fullmatch(r"(\d+)\.\.\.?", item):
+            yield unbound_range(int(match.group(1)))
+        else:
+            raise ValueError(f"Bad range: {item}")
 
 
 def stream_file_field(
