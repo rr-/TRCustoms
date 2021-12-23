e9bf497cf6010c86af87199d8695d997dea88018
rr- <dash@wind.garden>
2021-12-23 19:46:04 +0100

    misc: stylize sidebars

--- frontend/src/components/pages/ProfilePage.tsx
+++ frontend/src/components/pages/ProfilePage.tsx
@@ -7,6 +7,9 @@ import { UserService } from "src/services/user.service";
 import LevelsTable from "src/shared/components/LevelsTable";
 import Loader from "src/shared/components/Loader";
 import { Markdown } from "src/shared/components/Markdown";
+import { PermissionGuard } from "src/shared/components/PermissionGuard";
+import PushButton from "src/shared/components/PushButton";
+import SidebarBox from "src/shared/components/SidebarBox";
 import UserLink from "src/shared/components/UserLink";
 import UserPicture from "src/shared/components/UserPicture";
 import { DISABLE_PAGING } from "src/shared/constants";
@@ -41,27 +44,38 @@ const ProfilePage = () => {
 
   return (
     <div id="ProfilePage">
-      <aside>
+      <header id="ProfilePage--header">
+        <h1>{user.username}</h1>
+        {user.is_active &&
+          `${user.first_name} ${user.last_name}` !== user.username && (
+            <h2>
+              {user.first_name} {user.last_name}
+            </h2>
+          )}
+      </header>
+
+      <aside id="ProfilePage--sidebar">
         <UserPicture user={user} />
-        <dl>
-          <dt>Joined</dt>
-          <dd>{formatDateTime(user.date_joined) || "unknown"}</dd>
 
-          <dt>Last seen</dt>
-          <dd>{formatDateTime(user.last_login) || "never"}</dd>
-        </dl>
+        <SidebarBox
+          actions={
+            <PermissionGuard require={"canEditUsers"} entity={user}>
+              <PushButton to={`/users/{user.id}/edit`}>Edit profile</PushButton>
+            </PermissionGuard>
+          }
+        >
+          <dl>
+            <dt>Joined</dt>
+            <dd>{formatDateTime(user.date_joined) || "unknown"}</dd>
 
-        <UserLink variant="edit" user={user} label="Edit profile" />
+            <dt>Last seen</dt>
+            <dd>{formatDateTime(user.last_login) || "never"}</dd>
+          </dl>
+        </SidebarBox>
       </aside>
-      <div>
-        <section className="ProfilePage--basic-info">
-          <h1>{user.username}</h1>
-          {user.is_active &&
-            `${user.first_name} ${user.last_name}` !== user.username && (
-              <h2>
-                {user.first_name} {user.last_name}
-              </h2>
-            )}
+
+      <div id="ProfilePage--main">
+        <section id="ProfilePage--basicInfo">
           {user.is_active && user.bio ? (
             <Markdown children={user.bio} />
           ) : (
@@ -69,7 +83,7 @@ const ProfilePage = () => {
           )}
         </section>
 
-        <section className="ProfilePage--authored-levels">
+        <section id="ProfilePage--authoredLevels">
           <h3>Authored levels</h3>
           <LevelsTable query={levelQuery} />
         </section>
