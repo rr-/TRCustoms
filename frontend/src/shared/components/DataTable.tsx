6c1ecd2dfc9e23cb08b22802c46308a96f7834ab
Dash <dash@bad.pink>
2021-12-31 21:39:45 +0100

    data table: split into components

--- frontend/src/shared/components/DataTable.tsx
+++ frontend/src/shared/components/DataTable.tsx
@@ -5,36 +5,110 @@ import Pager from "src/shared/components/Pager";
 import SortLink from "src/shared/components/SortLink";
 import type { PagedResponse } from "src/shared/types";
 
-interface DataTableColumn<TDataTableItem> {
+interface DataTableColumn<TItem> {
   name: string;
   tooltip?: string | null;
   sortKey?: string | null;
   label: string;
-  itemTooltip?: (item: TDataTableItem) => string | null;
-  itemElement: (item: TDataTableItem) => React.ReactElement | string;
+  itemTooltip?: (item: TItem) => string | null;
+  itemElement: (item: TItem) => React.ReactElement | string;
   footer?: () => React.ReactElement | string;
 }
 
-interface DataTableProps<TDataTableItem> {
+interface DataTableProps<TItem> {
   className?: string | null;
-  result: UseQueryResult<PagedResponse<TDataTableItem>, Error> | null;
-  itemKey: (TDataTableItem) => string;
-  columns: DataTableColumn<TDataTableItem>[];
+  result: UseQueryResult<PagedResponse<TItem>, Error> | null;
+  itemKey: (TItem) => string;
+  columns: DataTableColumn<TItem>[];
 
   sort?: string | null;
   onSortChange?: (sort: string) => any | null;
   onPageChange?: (page: number) => any | null;
 }
 
-const DataTable = <TDataTableItem extends {}>({
+const DataTableHeader = <TItem extends {}>({
+  className,
+  columns,
+  onSortChange,
+  sort,
+}: DataTableProps<TItem>) => {
+  return (
+    <thead>
+      <tr>
+        {columns.map((column) => (
+          <th
+            className={className && `${className}--${column.name}`}
+            title={column.tooltip}
+            key={`head-${column.name}`}
+          >
+            {onSortChange && column.sortKey ? (
+              <SortLink
+                currentSort={sort}
+                onSortChange={onSortChange}
+                targetSort={column.sortKey}
+              >
+                {column.label}
+              </SortLink>
+            ) : (
+              column.label
+            )}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+};
+
+const DataTableBody = <TItem extends {}>({
   className,
   result,
   itemKey,
   columns,
-  sort,
-  onSortChange,
-  onPageChange,
-}: DataTableProps<TDataTableItem>) => {
+}: DataTableProps<TItem>) => {
+  return (
+    <tbody>
+      {result.data.results.map((item) => (
+        <tr key={itemKey(item)}>
+          {columns.map((column) => (
+            <td
+              className={className && `${className}--${column.name}`}
+              title={column.itemTooltip?.(item)}
+              key={`${itemKey(item)}-${column.name}`}
+            >
+              {column.itemElement(item)}
+            </td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+const DataTableFooter = <TItem extends {}>({
+  className,
+  columns,
+}: DataTableProps<TItem>) => {
+  return (
+    columns.some((column) => !!column.footer) && (
+      <tfoot>
+        <tr>
+          {columns.map((column) => (
+            <th
+              className={className && `${className}--${column.name}`}
+              key={`foot-${column.name}`}
+            >
+              {column.footer?.()}
+            </th>
+          ))}
+        </tr>
+      </tfoot>
+    )
+  );
+};
+
+const DataTable = <TItem extends {}>(props: DataTableProps<TItem>) => {
+  const { className, result, onPageChange } = props;
+
   if (result.error) {
     return <p>{result.error.message}</p>;
   }
@@ -47,70 +121,17 @@ const DataTable = <TDataTableItem extends {}>({
     return <p>There are no result to show.</p>;
   }
 
-  if (!className) {
-    className = "DataTable";
-  }
-
   return (
     <>
       <table className={`DataTable ${className} borderless`}>
-        <thead>
-          <tr>
-            {columns.map((column) => (
-              <th
-                className={`${className}--${column.name}`}
-                title={column.tooltip}
-                key={`head-${column.name}`}
-              >
-                {onSortChange && column.sortKey ? (
-                  <SortLink
-                    currentSort={sort}
-                    onSortChange={onSortChange}
-                    targetSort={column.sortKey}
-                  >
-                    {column.label}
-                  </SortLink>
-                ) : (
-                  column.label
-                )}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {result.data.results.map((item) => (
-            <tr key={itemKey(item)}>
-              {columns.map((column) => (
-                <td
-                  className={`${className}--${column.name}`}
-                  title={column.itemTooltip?.(item)}
-                  key={`${itemKey(item)}-${column.name}`}
-                >
-                  {column.itemElement(item)}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-        {columns.some((column) => !!column.footer) && (
-          <tfoot>
-            <tr>
-              {columns.map((column) => (
-                <th
-                  className={`${className}--${column.name}`}
-                  key={`foot-${column.name}`}
-                >
-                  {column.footer?.()}
-                </th>
-              ))}
-            </tr>
-          </tfoot>
-        )}
+        <DataTableHeader {...props} />
+        <DataTableBody {...props} />
+        <DataTableFooter {...props} />
       </table>
       {onPageChange && !result.data.disable_paging && (
         <Pager
           onPageChange={onPageChange}
-          className={`${className}--pager`}
+          className={className && `${className}--pager`}
           pagedResponse={result.data}
         />
       )}
