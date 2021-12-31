760d003c66810777f8a9e68f7fd78ce0ce60200e
rr- <dash@wind.garden>
2021-12-31 23:35:45 +0100

    data table: add infinite scroll

--- frontend/src/shared/components/DataTable.tsx
+++ frontend/src/shared/components/DataTable.tsx
@@ -1,11 +1,19 @@
 import "./DataTable.css";
-import { UseQueryResult } from "react-query";
+import { Fragment } from "react";
+import { useRef } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
 import Loader from "src/shared/components/Loader";
 import Pager from "src/shared/components/Pager";
 import SortLink from "src/shared/components/SortLink";
+import { DISABLE_PAGING } from "src/shared/constants";
 import type { GenericSearchResult } from "src/shared/types";
 import type { GenericSearchQuery } from "src/shared/types";
+import { createLocalStorageStateHook } from "use-local-storage-state";
+
+const useInfiniteScroll = createLocalStorageStateHook("infiniteScroll", false);
 
 interface DataTableColumn<TItem> {
   name: string;
@@ -68,10 +76,16 @@ const DataTableHeader = <TItem extends {}, TQuery extends GenericSearchQuery>({
 const DataTableBody = <TItem extends {}, TQuery extends GenericSearchQuery>({
   className,
   result,
+  lastRowRef,
   itemKey,
   columns,
 }: {
-  result: UseQueryResult<GenericSearchResult<TQuery, TItem>, Error>;
+  result: {
+    isLoading?: boolean;
+    data?: GenericSearchResult<TQuery, TItem>;
+    error?: Error;
+  };
+  lastRowRef?: any;
 } & DataTableProps<TItem, TQuery>) => {
   if (result.error) {
     return (
@@ -108,7 +122,7 @@ const DataTableBody = <TItem extends {}, TQuery extends GenericSearchQuery>({
   return (
     <tbody>
       {result.data.results.map((item) => (
-        <tr key={itemKey(item)}>
+        <tr key={itemKey(item)} ref={lastRowRef}>
           {columns.map((column) => (
             <td
               className={className && `${className}--${column.name}`}
@@ -146,7 +160,7 @@ const DataTableFooter = <TItem extends {}, TQuery extends GenericSearchQuery>({
   );
 };
 
-const DataTable = <TItem extends {}, TQuery extends GenericSearchQuery>(
+const PagedDataTable = <TItem extends {}, TQuery extends GenericSearchQuery>(
   props: DataTableProps<TItem, TQuery>
 ) => {
   const { className, searchQuery, onSearchQueryChange, searchFunc } = props;
@@ -179,5 +193,111 @@ const DataTable = <TItem extends {}, TQuery extends GenericSearchQuery>(
   );
 };
 
+const InfiniteDataTable = <TItem extends {}, TQuery extends GenericSearchQuery>(
+  props: DataTableProps<TItem, TQuery>
+) => {
+  const { className, searchQuery, searchFunc } = props;
+
+  const [loadingElement, setLoadingElement] = useState(null);
+  const result = useInfiniteQuery<
+    GenericSearchResult<TQuery, TItem> | null,
+    Error
+  >(
+    [className /* hack: using class name to cache queries */, searchQuery],
+    async ({ pageParam }) => {
+      return searchFunc({
+        ...searchQuery,
+        page:
+          searchQuery.page === DISABLE_PAGING
+            ? DISABLE_PAGING
+            : pageParam === undefined
+            ? 1
+            : pageParam,
+      });
+    },
+    {
+      getNextPageParam: (lastPage, pages) => {
+        return lastPage.current_page < lastPage.last_page
+          ? lastPage.current_page + 1
+          : undefined;
+      },
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  const observer = useRef(
+    new IntersectionObserver((entries) => {
+      const first = entries[0];
+      if (first.isIntersecting) {
+        result.fetchNextPage();
+      }
+    })
+  );
+
+  useEffect(() => {
+    const currentElement = loadingElement;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [loadingElement]);
+
+  return (
+    <>
+      <table className={`DataTable ${className} borderless`}>
+        <DataTableHeader {...props} />
+
+        {result.data?.pages?.map((result, i) => (
+          <Fragment key={`body${i}`}>
+            <DataTableBody
+              lastRowRef={setLoadingElement}
+              result={{ isLoading: false, data: result, error: null }}
+              {...props}
+            />
+          </Fragment>
+        ))}
+
+        <tfoot>
+          <tr>
+            <td colSpan={100}>
+              <div>
+                <span
+                  style={{
+                    display:
+                      result.isFetching || result.isFetchingNextPage
+                        ? "block"
+                        : "none",
+                  }}
+                >
+                  Loading…
+                </span>
+              </div>
+            </td>
+          </tr>
+        </tfoot>
+
+        <DataTableFooter {...props} />
+      </table>
+    </>
+  );
+};
+
+const DataTable = <TItem extends {}, TQuery extends GenericSearchQuery>(
+  props: DataTableProps<TItem, TQuery>
+) => {
+  const [enableInfiniteScroll] = useInfiniteScroll();
+  if (enableInfiniteScroll) {
+    return <InfiniteDataTable {...props} />;
+  }
+  return <PagedDataTable {...props} />;
+};
+
 export type { DataTableColumn };
-export { DataTable };
+export { useInfiniteScroll, PagedDataTable, InfiniteDataTable, DataTable };
