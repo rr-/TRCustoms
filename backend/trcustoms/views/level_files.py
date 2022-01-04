498b30108f32cf82970bdf844c34d2456c662082
rr- <dash@wind.garden>
2022-01-04 22:51:29 +0100

    files: fix retrieving objects with /null/

--- backend/trcustoms/views/level_files.py
+++ backend/trcustoms/views/level_files.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
--- backend/trcustoms/views/uploads.py
+++ backend/trcustoms/views/uploads.py
@@ -1,6 +1,6 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
