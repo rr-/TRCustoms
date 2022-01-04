2fce14b00229ffb94c29dfe560cc5f0c35c8d38c
rr- <dash@wind.garden>
2022-01-04 23:27:10 +0100

    files: require images

--- backend/trcustoms/serializers/uploaded_files.py
+++ backend/trcustoms/serializers/uploaded_files.py
@@ -9,6 +9,13 @@ MAX_SIZE_MAP = {
     UploadedFile.UploadType.LEVEL_FILE: 1024 * 1024 * 1024,  # 1 GB
 }
 
+CONTENT_TYPE_MAP = {
+    UploadedFile.UploadType.USER_PICTURE: ["image/jpeg", "image/png"],
+    UploadedFile.UploadType.LEVEL_BANNER: ["image/jpeg"],
+    UploadedFile.UploadType.LEVEL_SCREENSHOT: ["image/jpeg"],
+    UploadedFile.UploadType.LEVEL_FILE: ["application/zip"],
+}
+
 
 class UploadedFileSerializer(serializers.ModelSerializer):
     url = serializers.SerializerMethodField(read_only=True)
@@ -20,6 +27,7 @@ class UploadedFileSerializer(serializers.ModelSerializer):
     def validate(self, data):
         try:
             max_file_size = MAX_SIZE_MAP[data["upload_type"]]
+            allowed_content_types = CONTENT_TYPE_MAP[data["upload_type"]]
         except KeyError:
             raise serializers.ValidationError(
                 {
@@ -32,6 +40,20 @@ class UploadedFileSerializer(serializers.ModelSerializer):
 
         if not data.get("content"):
             raise serializers.ValidationError({"content": "Missing file."})
+
+        if data["content"].content_type not in allowed_content_types:
+            allowed_content_types_readable = [
+                content_type.split("/")[1].upper()
+                for content_type in allowed_content_types
+            ]
+            raise serializers.ValidationError(
+                {
+                    "content": (
+                        "Invalid file type. Allowed types: "
+                        f"{', '.join(allowed_content_types_readable)}"
+                    )
+                }
+            )
 
         if data["content"].size > max_file_size:
             raise serializers.ValidationError(
