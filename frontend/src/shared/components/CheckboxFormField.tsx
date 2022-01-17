3454f9544a3a59f18e5857c29ce7e33e59d43df8
rr- <dash@wind.garden>
2022-01-17 21:22:03 +0100

    levels: add mod-only filter by approval status

---
+++ frontend/src/shared/components/CheckboxFormField.tsx
@@ -0,0 +1,34 @@
+import { Field } from "formik";
+import { useFormikContext } from "formik";
+import { BaseFormField } from "src/shared/components/BaseFormField";
+import type { GenericFormFieldProps } from "src/shared/components/BaseFormField";
+
+const CheckboxFormField = ({
+  name,
+  label,
+  readonly,
+  ...props
+}: GenericFormFieldProps) => {
+  const { values, setFieldValue } = useFormikContext() as {
+    values: { [key: string]: any };
+    setFieldValue: (name: string, value: any) => void;
+  };
+
+  return (
+    <BaseFormField name={name} readonly={readonly} label={null} {...props}>
+      <label>
+        <Field
+          disabled={readonly}
+          type="checkbox"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setFieldValue(name, event.target.checked);
+          }}
+          checked={values[name]}
+        />
+        {label}
+      </label>
+    </BaseFormField>
+  );
+};
+
+export { CheckboxFormField };
