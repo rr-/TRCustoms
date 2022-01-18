f9e3760f5265edf88ffe1c199fbcc71bcc590e34
Dash <dash@bad.pink>
2022-01-19 00:01:36 +0100

    levels: allow users to create new tags

--- backend/trcustoms/views/level_tags.py
+++ backend/trcustoms/views/level_tags.py
@@ -1,13 +1,34 @@
 from rest_framework import mixins, viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from trcustoms.mixins import MultiSerializerMixin, PermissionsMixin
 from trcustoms.models import LevelTag
-from trcustoms.serializers import LevelTagListingSerializer
+from trcustoms.permissions import AllowNone
+from trcustoms.serializers import (
+    LevelTagDetailsSerializer,
+    LevelTagListingSerializer,
+)
 
 
-class LevelTagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
-    permission_classes = [AllowAny]
+class LevelTagViewSet(
+    PermissionsMixin,
+    MultiSerializerMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = LevelTag.objects.with_counts()
-    serializer_class = LevelTagListingSerializer
     search_fields = ["name"]
     ordering_fields = ["name", "level_count", "created", "last_updated"]
+
+    permission_classes = [AllowNone]
+    permission_classes_by_action = {
+        "create": [IsAuthenticated],
+        "list": [AllowAny],
+        "retrieve": [IsAuthenticated],
+    }
+
+    serializer_class = LevelTagListingSerializer
+    serializer_class_by_action = {
+        "create": LevelTagDetailsSerializer,
+    }
--- frontend/src/services/tag.service.ts
+++ frontend/src/services/tag.service.ts
@@ -4,6 +4,7 @@ import { API_URL } from "src/shared/constants";
 import type { GenericSearchQuery } from "src/shared/types";
 import type { PagedResponse } from "src/shared/types";
 import { GenericSearchResult } from "src/shared/types";
+import { filterFalsyObjectValues } from "src/shared/utils";
 import { getGenericSearchQuery } from "src/shared/utils";
 
 interface TagLite {
@@ -31,8 +32,22 @@ const searchTags = async (
   return { ...response.data, searchQuery };
 };
 
+interface TagCreatePayload {
+  name?: string;
+}
+
+const create = async (payload: TagCreatePayload): Promise<Tag> => {
+  const data: { [key: string]: any } = filterFalsyObjectValues(payload);
+  const response = (await api.post(
+    `${API_URL}/level_tags/`,
+    data
+  )) as AxiosResponse<Tag>;
+  return response.data;
+};
+
 const TagService = {
   searchTags,
+  create,
 };
 
 export type { Tag, TagLite, TagList, TagSearchQuery, TagSearchResult };
