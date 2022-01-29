f3fe2eb8281eceec4ffe1a5ae8e7a5407863acfd
rr- <dash@wind.garden>
2022-01-29 20:03:00 +0100

    reviews: improve appearance

--- frontend/src/shared/components/links/UserLink.tsx
+++ frontend/src/shared/components/links/UserLink.tsx
@@ -4,16 +4,23 @@ import { UserPermission } from "src/services/user.service";
 import { PermissionGuard } from "src/shared/components/PermissionGuard";
 
 interface UserLinkProps {
+  className?: string | undefined;
   user: UserNested;
   children?: React.ReactNode | undefined;
 }
 
-const UserLink = ({ user, children }: UserLinkProps) => {
+const UserLink = ({ user, className, children }: UserLinkProps) => {
   const { id, username } = user;
   children ||= username;
   return (
     <PermissionGuard require={UserPermission.listUsers} alternative={username}>
-      {id ? <Link to={`/users/${id}`}>{children}</Link> : <>{children}</>}
+      {id ? (
+        <Link className={className} to={`/users/${id}`}>
+          {children}
+        </Link>
+      ) : (
+        <span className={className}>{children}</span>
+      )}
     </PermissionGuard>
   );
 };
