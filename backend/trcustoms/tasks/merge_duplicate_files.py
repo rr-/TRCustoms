bf635902d6bb33674d5761cc8c9d91c52ad36a13
Dash <dash@bad.pink>
2022-01-29 17:21:42 +0100

    backend: fix file deduper across multiple upload types

--- backend/trcustoms/tasks/merge_duplicate_files.py
+++ backend/trcustoms/tasks/merge_duplicate_files.py
@@ -1,25 +1,39 @@
 from django.db.models import Count
 
 from trcustoms.celery import app, logger
-from trcustoms.models import LevelFile, LevelScreenshot, UploadedFile, User
+from trcustoms.models import (
+    Level,
+    LevelFile,
+    LevelScreenshot,
+    UploadedFile,
+    User,
+)
 from trcustoms.utils import check_model_references
 
 
 @app.task
 def merge_duplicate_files() -> None:
     for result in (
-        UploadedFile.objects.values("md5sum")
+        UploadedFile.objects.values("md5sum", "upload_type")
         .annotate(dupe_count=Count("md5sum"))
         .filter(dupe_count__gt=1)
     ):
         md5sum = result["md5sum"]
         dupe_count = result["dupe_count"]
+        upload_type = result["upload_type"]
 
-        logger.info(f"{md5sum}: merging {dupe_count} dupes")
+        logger.info(
+            f"{md5sum}: merging {dupe_count} dupes of type {upload_type}"
+        )
 
-        chosen = UploadedFile.objects.filter(md5sum=md5sum).first()
-        rest = UploadedFile.objects.filter(md5sum=md5sum).exclude(pk=chosen.pk)
+        chosen = UploadedFile.objects.filter(
+            md5sum=md5sum, upload_type=upload_type
+        ).first()
+        rest = UploadedFile.objects.filter(
+            md5sum=md5sum, upload_type=upload_type
+        ).exclude(pk=chosen.pk)
 
+        Level.objects.filter(cover__in=rest).update(cover=chosen)
         LevelFile.objects.filter(file__in=rest).update(file=chosen)
         LevelScreenshot.objects.filter(file__in=rest).update(file=chosen)
         User.objects.filter(picture__in=rest).update(picture=chosen)
