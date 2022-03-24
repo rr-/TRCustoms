aaca463e3bc3ec81bc42788498876397bded0106
Dash <dash@bad.pink>
2022-03-24 20:01:15 +0100

    frontend: trigger the scroll less often

--- frontend/src/components/ScrollToTop.tsx
+++ frontend/src/components/ScrollToTop.tsx
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const location = useLocation();
+  const { pathname } = useLocation();
   useEffect(() => {
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
-  }, [location]);
+  }, [pathname]);
 
   return null;
 };
