cef5d9cceb5ad83b4b63a35c2bcb0b647b8c3600
Dash <dash@bad.pink>
2022-04-21 00:22:26 +0200

    forms: add wysiwyg buttons

---
+++ frontend/src/components/icons/IconMarkdownBold.tsx
@@ -0,0 +1,19 @@
+const IconMarkdownBold = () => {
+  return (
+    <svg
+      height="16"
+      viewBox="0 0 16 16"
+      version="1.1"
+      width="16"
+      className="Icon"
+    >
+      <path
+        fillRule="evenodd"
+        fill="currentColor"
+        d="M4 2a1 1 0 00-1 1v10a1 1 0 001 1h5.5a3.5 3.5 0 001.852-6.47A3.5 3.5 0 008.5 2H4zm4.5 5a1.5 1.5 0 100-3H5v3h3.5zM5 9v3h4.5a1.5 1.5 0 000-3H5z"
+      />
+    </svg>
+  );
+};
+
+export { IconMarkdownBold };
---
+++ frontend/src/components/icons/IconMarkdownQuote.tsx
@@ -0,0 +1,19 @@
+const IconMarkdownQuote = () => {
+  return (
+    <svg
+      height="16"
+      viewBox="0 0 16 16"
+      version="1.1"
+      width="16"
+      className="Icon"
+    >
+      <path
+        fillRule="evenodd"
+        fill="currentColor"
+        d="M1.75 2.5a.75.75 0 000 1.5h10.5a.75.75 0 000-1.5H1.75zm4 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm0 5a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zM2.5 7.75a.75.75 0 00-1.5 0v6a.75.75 0 001.5 0v-6z"
+      />
+    </svg>
+  );
+};
+
+export { IconMarkdownQuote };
