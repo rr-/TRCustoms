b78de1f5d9c960236f939152df8399307c086862
rr- <dash@wind.garden>
2022-04-21 00:22:26 +0200

    forms: add wysiwyg buttons

---
+++ frontend/src/components/icons/IconMarkdownItalic.tsx
@@ -0,0 +1,19 @@
+const IconMarkdownItalic = () => {
+  return (
+    <svg
+      height="16"
+      viewBox="0 0 16 16"
+      version="1.1"
+      width="16"
+      className="Icon"
+    >
+      <path
+        fillRule="evenodd"
+        fill="currentColor"
+        d="M6 2.75A.75.75 0 016.75 2h6.5a.75.75 0 010 1.5h-2.505l-3.858 9H9.25a.75.75 0 010 1.5h-6.5a.75.75 0 010-1.5h2.505l3.858-9H6.75A.75.75 0 016 2.75z"
+      />
+    </svg>
+  );
+};
+
+export { IconMarkdownItalic };
