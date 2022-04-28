258f600e3d3f044f362ef582f1ebb98bdbd03f31
rr- <dash@wind.garden>
2022-04-28 10:36:06 +0200

    markdown: add side by side preview

--- frontend/src/components/pages/SettingsPage.tsx
+++ frontend/src/components/pages/SettingsPage.tsx
@@ -4,11 +4,13 @@ import { useContext } from "react";
 import { Checkbox } from "src/components/Checkbox";
 import { ThemeSwitcher } from "src/components/ThemeSwitcher";
 import { useSettings } from "src/contexts/SettingsContext";
+import { MarkdownPreviewMode } from "src/contexts/SettingsContext";
 import { TitleContext } from "src/contexts/TitleContext";
 
 const SettingsPage = () => {
-  const { infiniteScroll, setInfiniteScroll } = useSettings();
   const { setTitle } = useContext(TitleContext);
+  const { infiniteScroll, setInfiniteScroll } = useSettings();
+  const { markdownPreviewMode, setMarkdownPreviewMode } = useSettings();
 
   useEffect(() => {
     setTitle("Settings");
@@ -25,6 +27,18 @@ const SettingsPage = () => {
         label="Enable infinite scroll"
         onChange={(e) => setInfiniteScroll(e.target.checked)}
         checked={infiniteScroll}
+      />
+
+      <Checkbox
+        label="Enable side-by-side Markdown preview"
+        onChange={(e) =>
+          setMarkdownPreviewMode(
+            markdownPreviewMode === MarkdownPreviewMode.Tabbed
+              ? MarkdownPreviewMode.SideBySide
+              : MarkdownPreviewMode.Tabbed
+          )
+        }
+        checked={markdownPreviewMode === MarkdownPreviewMode.SideBySide}
       />
     </div>
   );
