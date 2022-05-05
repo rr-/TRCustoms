e3cb04e1f58746d5687b768ee259af6555ee8dae
Dash <dash@bad.pink>
2022-05-05 19:20:32 +0200

    homepage: limit percentage decimal places to 1

--- frontend/src/utils/string.ts
+++ frontend/src/utils/string.ts
@@ -2,17 +2,17 @@ import { round } from "lodash";
 
 const EMPTY_INPUT_PLACEHOLDER = "-";
 
-const reprBigNumber = (num: number): string => {
+const reprBigNumber = (num: number, decimalPlaces = 1): string => {
   const suffixes = ["", "k", "m"];
   let suffix = suffixes.shift();
   while (suffixes.length && num >= 1000) {
     num /= 1000;
     suffix = suffixes.shift();
   }
-  return `${round(num, 1)}${suffix}`;
+  return `${round(num, decimalPlaces)}${suffix}`;
 };
 
-const reprPercentage = (num: number, decimalPlaces = 2): string => {
+const reprPercentage = (num: number, decimalPlaces = 1): string => {
   return `${round(num * 100, decimalPlaces)}%`;
 };
 
