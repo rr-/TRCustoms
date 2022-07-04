98a503135e98b423a1fcb3f7f2ff013a1cb38555
rr- <dash@wind.garden>
2022-07-04 19:16:30 +0200

    featured-levels: fix best in genre calculation
    Regression from 16f088d

--- backend/trcustoms/tasks/update_featured_levels.py
+++ backend/trcustoms/tasks/update_featured_levels.py
@@ -170,7 +170,7 @@ def update_best_level_in_genre() -> FeaturedLevel | None:
         genre = Genre.objects.get(name=genre_name)
         visited_genre_names.add(genre_name)
 
-        levels = Level.objects.all(genres=genre)
+        levels = Level.objects.filter(genres=genre)
 
         # make sure we pick only the best rated levels
         levels = filter_by_rating_class(
