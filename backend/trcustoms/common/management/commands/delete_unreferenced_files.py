343e2689e1b38faaae345c0b8cffb47c3426d003
Dash <dash@bad.pink>
2022-11-25 21:58:12 +0100

    commands: add --dry-run to dangling files cleanup

--- backend/trcustoms/common/management/commands/delete_unreferenced_files.py
+++ backend/trcustoms/common/management/commands/delete_unreferenced_files.py
@@ -6,5 +6,8 @@ from trcustoms.tasks import delete_unreferenced_files
 class Command(BaseCommand):
     help = "Delete unreferenced files."
 
+    def add_arguments(self, parser):
+        parser.add_argument("-d", "--dry-run", action="store_true")
+
     def handle(self, *args, **options):
-        delete_unreferenced_files()
+        delete_unreferenced_files(dry_run=options["dry_run"])
