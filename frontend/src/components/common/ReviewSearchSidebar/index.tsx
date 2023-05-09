4e1eede7ff240221daa333878a1116b4386103ba
Dash <dash@bad.pink>
2023-05-09 13:40:14 +0200

    reviews: fix too busy review sidebar

--- frontend/src/components/common/ReviewSearchSidebar/index.tsx
+++ frontend/src/components/common/ReviewSearchSidebar/index.tsx
@@ -8,25 +8,19 @@ const ReviewSearchSidebar = () => {
   return (
     <SidebarBox>
       <Section className="ChildMarginClear">
-        <SectionHeader>Reviewer catalogue</SectionHeader>
+        <SectionHeader>Reviewer longue</SectionHeader>
+
         <Link to={`/reviews/authors`}>
           <GFXCard name="reviewer_catalogue" variant="big">
             Reviewer catalogue
           </GFXCard>
         </Link>
-      </Section>
 
-      <Section className="ChildMarginClear">
-        <SectionHeader>Least reviewed levels</SectionHeader>
         <Link to={`/reviews/level_suggestions`}>
           <GFXCard name="least_reviewed_levels" variant="big">
             Less known levels
           </GFXCard>
         </Link>
-        <p>
-          A list of levels with less than 5 reviews, sorted by the oldest
-          release date.
-        </p>
       </Section>
     </SidebarBox>
   );
