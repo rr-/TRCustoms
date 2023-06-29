f4cab74eb9139f469fe38c85211d9a4a57cdfbe4
Marcin Kurczewski <dash@wind.garden>
2023-06-29 17:48:27 +0200

    frontend: extract home layout

--- frontend/src/components/pages/HomePage/index.tsx
+++ frontend/src/components/pages/HomePage/index.tsx
@@ -1,5 +1,5 @@
-import styles from "./index.module.css";
 import { NewsSidebar } from "src/components/common/NewsSidebar";
+import { HomeLayout } from "src/components/layouts/HomeLayout";
 import { FeaturedLevelsView } from "src/components/pages/HomePage/FeaturedLevels";
 import { StatsSidebar } from "src/components/pages/HomePage/StatsSidebar";
 import { usePageMetadata } from "src/contexts/PageMetadataContext";
@@ -8,17 +8,9 @@ const HomePage = () => {
   usePageMetadata(() => ({ ready: true, title: "" }), []);
 
   return (
-    <div className={styles.page}>
-      <div className={styles.featuredLevels}>
-        <FeaturedLevelsView />
-      </div>
-      <aside className={styles.leftSidebar}>
-        <StatsSidebar />
-      </aside>
-      <aside className={styles.rightSidebar}>
-        <NewsSidebar />
-      </aside>
-    </div>
+    <HomeLayout left={<StatsSidebar />} right={<NewsSidebar />}>
+      <FeaturedLevelsView />
+    </HomeLayout>
   );
 };
 
