8828eb304ca8f4e31cbfb936c1c526955f6ec2cc
Dash <dash@bad.pink>
2023-07-10 00:59:12 +0200

    uploads: move signals from common

--- backend/trcustoms/uploads/apps.py
+++ backend/trcustoms/uploads/apps.py
@@ -1,6 +1,10 @@
+# pylint: disable=W0611,C0415
 from django.apps import AppConfig
 
 
 class UploadsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "trcustoms.uploads"
+
+    def ready(self):
+        import trcustoms.uploads.signals  # noqa: F401
