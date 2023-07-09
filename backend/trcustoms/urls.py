0c6632d486474f78e0add3de7f93da6c01378436
Dash <dash@bad.pink>
2023-07-09 22:24:19 +0200

    backend: add spectacular

--- backend/trcustoms/urls.py
+++ backend/trcustoms/urls.py
@@ -2,6 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -50,6 +55,17 @@ urlpatterns = [
         "api/users/<int:user_id>/playlist/<int:pk>/",
         as_detail_view(PlaylistItemViewSet),
         name="user-playlist-detail",
+    ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="api:common:schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
 ]
 
