8ef9f945487d13eafe81241d416a5735967e0d52
Marcin Kurczewski <dash@wind.garden>
2024-04-28 18:11:37 +0200

    news: remove text length limit

--- backend/trcustoms/news/models.py
+++ backend/trcustoms/news/models.py
@@ -7,7 +7,7 @@ from trcustoms.common.models import DatesInfo
 @registry.register_model(name_getter=lambda instance: instance.subject)
 class News(DatesInfo):
     subject = models.TextField(max_length=200, null=True, blank=True)
-    text = models.TextField(max_length=5000, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
 
     class Meta:
         ordering = ["-created"]
