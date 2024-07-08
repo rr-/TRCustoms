b4dbf74a19d4ac0c7acfd50e509f0a9806221d50
Marcin Kurczewski <dash@wind.garden>
2024-07-08 15:26:40 +0200

    misc: update pre-commit

--- services/prerender/server.js
+++ services/prerender/server.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
-const prerender = require('prerender');
-const fsCache = require('./plugins/filesystem-cache');
+const prerender = require("prerender");
+const fsCache = require("./plugins/filesystem-cache");
 
 // borrow from https://github.com/prerender/prerender/blob/master/server.js
 var server = prerender({
-  chromeLocation: '/usr/lib/chromium/chrome',
+  chromeLocation: "/usr/lib/chromium/chrome",
   chromeFlags: [
-    '--headless',
+    "--headless",
     //"--disable-gpu",
-    '--remote-debugging-port=9222',
+    "--remote-debugging-port=9222",
     //"--hide-scrollbars",
-    '--no-sandbox',
-    '--disk-cache-dir=/tmp/chromium',
+    "--no-sandbox",
+    "--disk-cache-dir=/tmp/chromium",
   ],
 });
 
