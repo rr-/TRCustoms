a00a1cc3589ea905aef499ef7c720a9514160c1f
Dash <dash@bad.pink>
2024-11-07 10:40:30 +0100

    frontend: add lint job

--- services/frontend/entrypoint.sh
+++ services/frontend/entrypoint.sh
@@ -9,6 +9,9 @@ Commands
 
 dev         : Run a development server
 shell       : Start a bash shell
+build       : Build the frontend
+tsc         : Run TypeScript compiler
+lint        : Run linters
 help        : Show this message
 """
 }
@@ -19,6 +22,15 @@ case "$1" in
     ;;
     shell)
         /bin/bash "${@:2}"
+    ;;
+    build)
+        npm run build
+    ;;
+    tsc)
+        npm run tsc
+    ;;
+    lint)
+        npm run lint
     ;;
     *)
         show_help
