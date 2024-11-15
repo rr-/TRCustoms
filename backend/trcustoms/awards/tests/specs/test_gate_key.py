1e1e78de1f448cb476300569388272377e248c10
Marcin Kurczewski <dash@wind.garden>
2024-11-15 01:31:55 +0100

    awards: fix gate key

--- backend/trcustoms/awards/tests/specs/test_gate_key.py
+++ backend/trcustoms/awards/tests/specs/test_gate_key.py
@@ -1,8 +1,30 @@
+from unittest.mock import patch
+
 import pytest
+from rest_framework import status
 
 from trcustoms.awards.specs import AwardSpec, gate_key
+from trcustoms.levels.models import Level
+from trcustoms.levels.tests.factories import LevelFactory
 from trcustoms.playlists.tests.factories import PlaylistItemFactory
+from trcustoms.users.models import User
 from trcustoms.users.tests.factories import UserFactory
+
+
+@pytest.fixture(name="create_playlist_item_via_api")
+def fixture_create_playlist_item_via_api(get_auth_api_client):
+    def factory(user: User, level: Level) -> dict:
+        response = get_auth_api_client(user).post(
+            f"/api/users/{user.pk}/playlist/",
+            format="json",
+            data={"level_id": level.pk, "status": "playing"},
+        )
+        assert (
+            response.status_code == status.HTTP_201_CREATED
+        ), response.content
+        return response.json()
+
+    return factory
 
 
 @pytest.fixture(name="spec")
@@ -44,3 +66,27 @@ def test_gate_key_award_spec_success_not_enough_players(
     PlaylistItemFactory(user__username="unique1", level__authors=[user])
     PlaylistItemFactory(user__username="unique1", level__authors=[user])
     assert spec.requirement(user) is False
+
+
+@pytest.mark.django_db
+def test_gate_key_award_spec_revoking(
+    spec: AwardSpec, create_playlist_item_via_api, get_auth_api_client
+) -> None:
+    with patch("trcustoms.awards.logic.ALL_AWARD_SPECS", [spec]):
+        author = UserFactory()
+        level = LevelFactory(authors=[author])
+
+        player1 = UserFactory(username="unique1")
+        player2 = UserFactory(username="unique2")
+        create_playlist_item_via_api(player1, level)
+        playlist_item2 = create_playlist_item_via_api(player2, level)
+
+        prev_award_count = author.awards.count()
+
+        get_auth_api_client(player2).delete(
+            f"/api/users/{player2.pk}/playlist/{playlist_item2['id']}/",
+        )
+        current_award_count = author.awards.count()
+
+    assert prev_award_count == 1
+    assert current_award_count == 0
--- backend/trcustoms/playlists/signals.py
+++ backend/trcustoms/playlists/signals.py
@@ -2,18 +2,26 @@ from django.db.models.signals import post_delete, post_save, pre_delete
 from django.dispatch import receiver
 
 from trcustoms.playlists.models import PlaylistItem
+from trcustoms.tasks import update_awards
 
 
 @receiver(post_save, sender=PlaylistItem)
 def update_level_player_on_playlist_item_change(sender, instance, **kwargs):
     instance.user.update_played_level_count()
+    for author in instance.level.authors.iterator():
+        update_awards.delay(author.pk)
+    update_awards.delay(instance.user.pk)
 
 
 @receiver(pre_delete, sender=PlaylistItem)
 def remember_playlist_item_user(sender, instance, using, **kwargs):
     instance._old_user = instance.user
+    instance._old_level = instance.level
 
 
 @receiver(post_delete, sender=PlaylistItem)
 def update_level_player_on_playlist_item_delete(sender, instance, **kwargs):
     instance._old_user.update_played_level_count()
+    for author in instance._old_level.authors.iterator():
+        update_awards.delay(author.pk)
+    update_awards.delay(instance._old_user.pk)
