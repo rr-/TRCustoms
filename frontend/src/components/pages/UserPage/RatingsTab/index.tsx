a6d05fda47f3f8ca20c1a992226a9ddda5365f01
Dash <dash@bad.pink>
2024-11-24 23:56:36 +0100

    users: redesign to use tabs

---
+++ frontend/src/components/pages/UserPage/RatingsTab/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { RatingsTable } from "src/components/common/RatingsTable";
+import type { RatingSearchQuery } from "src/services/RatingService";
+import type { UserDetails } from "src/services/UserService";
+
+const getRatingSearchQuery = (userId: number): RatingSearchQuery => ({
+  authors: [userId],
+  page: null,
+  sort: "-created",
+  search: "",
+});
+
+interface RatingsTabProps {
+  user: UserDetails;
+  isLoggedIn: boolean;
+}
+
+const RatingsTab = ({ user, isLoggedIn }: RatingsTabProps) => {
+  const [ratingSearchQuery, setRatingSearchQuery] = useState<RatingSearchQuery>(
+    getRatingSearchQuery(user.id)
+  );
+
+  useEffect(() => {
+    setRatingSearchQuery(getRatingSearchQuery(user.id));
+  }, [user.id, isLoggedIn]);
+
+  return (
+    <RatingsTable
+      showAuthors={false}
+      showLevels={true}
+      searchQuery={ratingSearchQuery}
+      onSearchQueryChange={setRatingSearchQuery}
+    />
+  );
+};
+
+export { RatingsTab };
